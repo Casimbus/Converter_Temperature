7afc953917551bfd6c48f3a37a4aae7b3ddd87fa
Casimbus <danila.id17@gmail.com>
2025-04-12 15:43:22 +0200

    Add converting to celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,15 @@
 package app;
 
 public class Main {
+
+
+    public static double convFahrToCel(double fahr){
+        return (fahr - 32) / 1.8;
+    }
     public static void main(String[] args) {
         System.out.println("This is a temperature converting app.\nVersion 1.0");
+        double fahr = 75;
+        double celsius = convFahrToCel(fahr);
+        System.out.printf("The result is: %.2f celsius degrees", celsius);
     }
 }
